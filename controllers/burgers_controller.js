545ab46770506a7165b949ddc3696993d1e40b50
Leigh Fisher <leighfisher@Jamess-MacBook-Air-2.local>
2017-04-29 09:28:41 -0500

    fix routing issues

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,7 +16,7 @@ router.get('/', function(req, res) {
   });
 });
 
-router.post('/burgers', function(req, res) {
+router.post('/', function(req, res) {
   burger.insertOne([
     'burger_name'
   ], [
@@ -26,7 +26,7 @@ router.post('/burgers', function(req, res) {
   });
 });
 
-router.put('/burgers/:id', function(req, res) {
+router.put('/:id', function(req, res) {
   var condition = 'id = ' + req.params.id;
 
   burger.updateOne({
